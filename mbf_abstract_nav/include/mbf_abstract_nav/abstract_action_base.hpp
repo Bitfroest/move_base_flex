de1c213a4b5c15a7ad8ebfde2cd8ca71b0f3c9dc
corot <jsantossimon@gmail.com>
2020-06-09 20:09:15 +0900

    Move RobotInformation to mbf_utility, as it can be used generaly

--- mbf_abstract_nav/include/mbf_abstract_nav/abstract_action_base.hpp
+++ mbf_abstract_nav/include/mbf_abstract_nav/abstract_action_base.hpp
@@ -41,9 +41,10 @@
 
 #include <actionlib/server/action_server.h>
 #include <mbf_abstract_nav/MoveBaseFlexConfig.h>
-#include "mbf_abstract_nav/robot_information.h"
+#include <mbf_utility/robot_information.h>
 
-namespace mbf_abstract_nav{
+namespace mbf_abstract_nav
+{
 
 template <typename Action, typename Execution>
 class AbstractActionBase
@@ -62,7 +63,7 @@ class AbstractActionBase
 
   AbstractActionBase(
       const std::string& name,
-      const RobotInformation &robot_info,
+      const mbf_utility::RobotInformation &robot_info,
       const RunMethod run_method
   ) : name_(name), robot_info_(robot_info), run_(run_method){}
 
@@ -168,7 +169,7 @@ class AbstractActionBase
 
 protected:
   const std::string &name_;
-  const RobotInformation &robot_info_;
+  const mbf_utility::RobotInformation &robot_info_;
 
   RunMethod run_;
   boost::thread_group threads_;
